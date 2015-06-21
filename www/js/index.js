68ad67950ec2d79efac9b5017c9a3b2bedc8c025
Miguel Gutierrez <ryuuzakiupldr@gmail.com>
2015-06-21 02:56:48 -0500

    guarda el historial de busquedas en una base de datos SQLite

--- www/js/index.js
+++ www/js/index.js
@@ -1,49 +1,60 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+$('#aaa').click(function () {
+    $('#search').val("hola");
+});
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+$('#btnGraph').click(function () {
+    var db = window.openDatabase("Database", "1.0", "History", 20000);
+    db.transaction(addItem, errorCB, successCB);
 
-        console.log('Received Event: ' + id);
+});
+window.addEventListener('load', function() {
+    document.body.addEventListener('touchmove', function(e) {
+        e.preventDefault();
+    }, false);
+}, false);
+
+
+/* --- DATABASE INTERACTIONS --- */
+
+// Wait for device API libraries to load
+document.addEventListener("deviceready", onDeviceReady, false);
+// device APIs are available
+function onDeviceReady() {
+    var db = window.openDatabase("Database", "1.0", "History", 2000000);
+    db.transaction(createDB, errorCB, successCB);
+    db.transaction(loadHistory, errorCB, successCB);
+}
+// Populate the database
+function createDB(tx) {
+    tx.executeSql('DROP TABLE IF EXISTS HISTORY');
+    tx.executeSql('CREATE TABLE IF NOT EXISTS HISTORY (ID INTEGER PRIMARY KEY ASC, STRUCTURE)');
+}
+function loadHistory(tx) {
+    tx.executeSql ('SELECT * FROM HISTORY ORDER BY ID DESC LIMIT 10', [], querySuccess, errorCB);
+}
+function addItem(tx) {
+    var valSearch = $("#search").val();
+    tx.executeSql('INSERT INTO HISTORY (STRUCTURE) VALUES ("'+valSearch+'")');
+    tx.executeSql ('SELECT * FROM HISTORY ORDER BY ID DESC LIMIT 10', [], querySuccess, errorCB);
+}
+function dropDB(tx) {
+    tx.executeSql('DROP TABLE IF EXISTS HISTORY');
+}
+// Transaction error callback
+function errorCB(err) {
+    alert("Error processing SQL: "+err.code);
+}
+// Transaction success callback
+function successCB() {
+    alert("success!");
+}
+function querySuccess(tx, results) {
+    var len = results.rows.length;
+    $("#historyList").html('<li data-icon="delete"><a href="#" data-rel="close" class="ui-btn ui-btn-icon-right ui-icon-delete">Close menu</a></li>');
+    for(var i = 0; i < len; i ++) {
+        var listaActual = $("#historyList").html();
+        //alert("<li><a id='"+results.rows.item(i).STRUCTURE+"' class='historyItem' href='#'>"+results.rows.item(i).STRUCTURE+"</a></li>");
+        $("#historyList").html(listaActual+"<li><a id='"+results.rows.item(i).STRUCTURE+"' class='ui-btn ui-btn-icon-right ui-icon-carat-r historyItem' href='#'>"+results.rows.item(i).STRUCTURE+"</a></li>");
     }
-};
+    return false;
+}
